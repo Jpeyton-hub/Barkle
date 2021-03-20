4d41709347d4efdf4cdeb4f2b11e6c22c58b81a1
Jpeyton <jpwalk319@gmail.com>
2021-03-20 09:44:44 -0600

    updated file structure per suggestion

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
+$(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
+  $.get("/api/user_data").then(data => {
     $(".member-name").text(data.email);
   });
 });
