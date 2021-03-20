e3225adf96bf51ab420233d5c3faf892c6e682e0
Jpeyton <jpwalk319@gmail.com>
2021-03-20 16:33:37 -0600

    added events model

---
+++ models/events.js
@@ -0,0 +1,31 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-unused-vars */
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class events extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  events.init(
+    {
+      name: DataTypes.STRING,
+      date: DataTypes.STRING,
+      time: DataTypes.STRING,
+      location_id: DataTypes.INTEGER,
+      user_id: DataTypes.INTEGER,
+      dogs_id: DataTypes.INTEGER
+    },
+    {
+      sequelize,
+      modelName: "events"
+    }
+  );
+  return events;
+};
