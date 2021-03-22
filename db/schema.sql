26dd8e45530ce0d64fae65584190cc5d6e3489c9
Jpeyton <jpwalk319@gmail.com>
2021-03-22 15:37:27 -0600

    added a seeds file with an insert for locations table, and the locations table to the schema file

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,10 @@ DROP DATABASE IF EXISTS barkle_db;
 
 CREATE DATABASE barkle_db;
 
+CREATE TABLE locations
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(225),
+    mapData VARCHAR(225),  
+    PRIMARY KEY(id)
+);
