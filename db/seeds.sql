bbadd96b8511f703092276beb0023dfd5f82f90c
Brandon Ashby <bash7325@gmail.com>
2021-03-24 10:59:50 -0600

    merge confilct did weird things, I think it's fixed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,3 @@
- Ariana_Branch1
 INSERT INTO locations VALUES ( name, mapData ) VALUES ( "Tanner Park", "2695 E Heritage Way, Salt Lake City, UT 84109");
-=======
+
 INSERT INTO locations ( name, city, state ) VALUES ( "Tanner Park", "Salt Lake City", "UT");
- main
