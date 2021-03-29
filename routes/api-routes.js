c7ee618f791251f7325638a8d89e70e2ed7d1f1a
Brandon Ashby <bash7325@gmail.com>
2021-03-29 13:33:24 -0600

    added error .hbs page and routing, UI changes, clean up html

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), (req, res) => {
+  app.post("/api/login", passport.authenticate("local", { failureRedirect: '/error'}), (req, res) => {
     // Sending back a password, even a hashed password, isn't a good idea
     if (!req.user) {
       res.redirect("/signup");
